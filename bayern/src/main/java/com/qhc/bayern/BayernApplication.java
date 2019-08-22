c4131d12849ee7e3ac3bc2f4a66cba1b80039c0d
Wayne <wang@dxc.com>
2019-08-22 11:50:48 +0800

    init

---
+++ bayern/src/main/java/com/qhc/bayern/BayernApplication.java
@@ -0,0 +1,13 @@
+package com.qhc.bayern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayernApplication.class, args);
+	}
+
+}
---
+++ bayern/src/test/java/com/qhc/bayern/BayernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qhc.bayern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
