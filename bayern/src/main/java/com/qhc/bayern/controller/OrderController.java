7e00a8e2526bb0a8156c79673ea023846f7ad46f
Wayne <wang@dxc.com>
2019-08-23 12:11:12 +0800

    order

--- bayern/src/main/java/com/qhc/bayern/controller/OrderController.java
+++ bayern/src/main/java/com/qhc/bayern/controller/OrderController.java
@@ -5,10 +5,14 @@ package com.qhc.bayern.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qhc.bayern.controller.entity.Order;
+
 import org.springframework.http.HttpStatus;
 
 import io.swagger.annotations.Api;
@@ -21,14 +25,14 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @Api(value = "Order Manager")
 @RequestMapping("/order")
-public class Order {
+public class OrderController {
 	
 	  @ApiOperation(value="push a new order to SAP")
-	  @PostMapping(value = "/push")
+	  @PostMapping(value = "/new")
 	  @ResponseStatus(HttpStatus.OK)
-	  public String getUserList(@RequestParam String number) throws Exception
+	  public String getUserList(@RequestBody Order order) throws Exception
 	  {
-		 return number;
+		 return order.code;
 	}
 
 }
---
+++ bayern/src/main/java/com/qhc/bayern/controller/entity/Order.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.qhc.bayern.controller.entity;
+
+/**
+ * @author wang@dxc.com
+ *
+ */
+public class Order {
+	public String code;
+
+}
