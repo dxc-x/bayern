e4a58546b1cb0af1dd83e3d59bb64c0c0a7629c1
Wayne <wang@dxc.com>
2019-09-03 17:32:40 +0800

    location

--- bayern/src/main/java/com/qhc/bayern/service/LocationService.java
+++ bayern/src/main/java/com/qhc/bayern/service/LocationService.java
@@ -46,22 +46,22 @@ public class LocationService extends AbsFryeService {
 	public List<SalesGroup> getSalesgroup() {
 		List<SalesGroup> rl = new ArrayList();
 		SalesGroup sg1 = new SalesGroup();
-		sg1.setCode("S001");
+		sg1.setCode("S01");
 		sg1.setName("青岛");
 		sg1.setOfficeCode("Z001");
 		sg1.setOfficeName("东区");
 		SalesGroup sg2 = new SalesGroup();
-		sg2.setCode("S002");
+		sg2.setCode("S02");
 		sg2.setName("济南");
 		sg2.setOfficeCode("Z001");
 		sg2.setOfficeName("东区");
 		SalesGroup sg3 = new SalesGroup();
-		sg3.setCode("S003");
+		sg3.setCode("S03");
 		sg3.setName("哈尔滨");
 		sg3.setOfficeCode("Z002");
 		sg3.setOfficeName("东北区");
 		SalesGroup sg4 = new SalesGroup();
-		sg4.setCode("S004");
+		sg4.setCode("S04");
 		sg4.setName("长春");
 		sg4.setOfficeCode("Z002");
 		sg4.setOfficeName("东北区");
