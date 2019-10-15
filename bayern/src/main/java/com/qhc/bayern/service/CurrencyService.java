a490220aed35cfa8e5a8a0012bdca6ceb5589dae
wangch <chunzhao.wang@carrier.com>
2019-10-15 09:10:45 +0800

    add 1

--- bayern/src/main/java/com/qhc/bayern/service/CurrencyService.java
+++ bayern/src/main/java/com/qhc/bayern/service/CurrencyService.java
@@ -8,11 +8,15 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.qhc.bayern.controller.entity.Currency;
 import com.qhc.bayern.controller.entity.Incoterm;
 import com.qhc.bayern.controller.entity.Price;
+import com.qhc.bayern.util.HttpUtil;
 
 /**
  * @author wang@dxc.com
@@ -24,6 +28,12 @@ public class CurrencyService {
 	private final static String PUT_CURRENCY = "currency";
 	private final static String PUT_INCOTERM = "currency/incoterm";
 	private final static String PUT_PRICE = "currency/price";
+	
+	@Value("${sap.currency.addr}")
+	String urlStr;
+	
+	@Value("${sap.currency.param}")
+	String param;
 
 	@Autowired
 	private FryeService<List<?>> fryeService;
@@ -43,20 +53,33 @@ public class CurrencyService {
 	 */
 	public List<Currency> getCurrencyFromSap(Date date) {
 		List<Currency> clist = new ArrayList<Currency>();
-		Currency c1 = new Currency();
-		c1.setCode("001");
-		c1.setName("dollar");
-		c1.setRate(32.80000);
-		c1.setEffective(date);
-		
-		Currency c2 = new Currency();
-		c2.setCode("002");
-		c2.setName("cre");
-		c2.setRate(0.25123);
-		c2.setEffective(date);
-		
-		clist.add(c1);
-		clist.add(c2);
+		try {
+			String bb = HttpUtil.postbody(urlStr, param);
+			JSONObject parseObject = JSONObject.parseObject(bb);
+			Object message = parseObject.get("message");
+			Object data = parseObject.get("data");
+			JSONArray parseArray = JSONArray.parseArray(data.toString());
+			for (int i = 0; i < parseArray.size();i++) { 
+//				  System.out.println(parseArray.get(i)); 
+				  JSONObject obj = (JSONObject)parseArray.get(i); 
+				  //
+				  if(obj.getString("tcurr").equals("CNY")) {
+					  System.out.println("不是对于人民币的汇率");
+					  break;
+				  }else {
+					  Currency currency = new Currency();
+					  currency.setCode(obj.getString("fcurr"));
+					  currency.setName(obj.getString("ktext_f"));
+					  
+					  currency.setRate(Double.valueOf(obj.getString("ukurs")));
+					  currency.setEffective(date);
+					  clist.add(currency);
+				  }
+				  
+			  }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return clist;
 	}
 	/**
