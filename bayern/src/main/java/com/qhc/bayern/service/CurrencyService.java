3b5ad97a28a5a9ecf13fc455b144bc40a5f38f4f
wangch <chunzhao.wang@carrier.com>
2019-10-16 15:45:04 +0800

    update 2

--- bayern/src/main/java/com/qhc/bayern/service/CurrencyService.java
+++ bayern/src/main/java/com/qhc/bayern/service/CurrencyService.java
@@ -15,6 +15,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.qhc.bayern.controller.entity.Currency;
 import com.qhc.bayern.controller.entity.Incoterm;
+import com.qhc.bayern.controller.entity.Parameter;
 import com.qhc.bayern.controller.entity.Price;
 import com.qhc.bayern.util.HttpUtil;
 import com.qhc.bayern.util.StrToDouble;
@@ -33,14 +34,8 @@ public class CurrencyService {
 	@Value("${sap.currency.addr}")
 	String currencyUrlStr;
 	
-	@Value("${sap.currency.param}")
-	String currencyParam;
-	
 	@Value("${sap.incoterm.addr}")
 	String incotermUrlStr;
-	
-	@Value("${sap.incoterm.param}")
-	String incotermParam;
 
 	@Autowired
 	private FryeService<List<?>> fryeService;
@@ -61,6 +56,18 @@ public class CurrencyService {
 	public List<Currency> getCurrencyFromSap(Date date) {
 		List<Currency> clist = new ArrayList<Currency>();
 		try {
+			//接口请求参数
+			Parameter parameter1 = new Parameter();
+			parameter1.setKey("KURST");
+			parameter1.setValue("M");
+			Parameter parameter2 = new Parameter();
+			parameter2.setKey("LANGU");
+			parameter2.setValue("1");
+			List<Parameter> parList = new ArrayList<Parameter>();
+			parList.add(parameter1);
+			parList.add(parameter2);
+			String currencyParam = JSONObject.toJSONString(parList);
+			//发送请求获取数据
 			String bb = HttpUtil.postbody(currencyUrlStr, currencyParam);
 			JSONObject parseObject = JSONObject.parseObject(bb);
 			Object message = parseObject.get("message");
@@ -70,19 +77,18 @@ public class CurrencyService {
 //				  System.out.println(parseArray.get(i)); 
 				  JSONObject obj = (JSONObject)parseArray.get(i); 
 				  //
-				  if(obj.getString("tcurr").equals("CNY")) {
-					  System.out.println("不是对于人民币的汇率");
-					  break;
+				  if(!obj.getString("tcurr").equals("CNY")|| obj.getString("ktext_f")=="") {
+					  System.out.println("数据有误");
+					  continue;
 				  }else {
 					  Currency currency = new Currency();
 					  currency.setCode(obj.getString("fcurr"));
-//					  currency.setName(obj.getString("ktext_f"));
-					  currency.setName("111");
+					  currency.setName(obj.getString("ktext_f"));
+//					  currency.setName("111");
 					  currency.setRate(StrToDouble.test(obj.getString("ukurs")));
 					  currency.setEffective(date);
 					  clist.add(currency);
 				  }
-				  
 			  }
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -106,6 +112,14 @@ public class CurrencyService {
 	public List<Incoterm> getIncotermFromSap() {
 		List<Incoterm> ilist = new ArrayList<Incoterm>();
 		try {
+			//接口请求参数
+			Parameter parameter1 = new Parameter();
+			parameter1.setKey("LANGU");
+			parameter1.setValue("1");
+			List<Parameter> parList = new ArrayList<Parameter>();
+			parList.add(parameter1);
+			String incotermParam = JSONObject.toJSONString(parList);
+			//发送请求获取数据
 			String bb = HttpUtil.postbody(incotermUrlStr, incotermParam);
 			JSONObject parseObject = JSONObject.parseObject(bb);
 			Object message = parseObject.get("message");
@@ -116,16 +130,13 @@ public class CurrencyService {
 				Incoterm incoterm = new Incoterm();
 				incoterm.setCode(obj.getString("inco1"));
 				incoterm.setName(obj.getString("bezei"));
-				incoterm.setSapSalesTypeCode("10");
+				//全部设置20-出口
+				incoterm.setSapSalesTypeCode("20");
 				ilist.add(incoterm);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		Incoterm i1 = new Incoterm();
-//		i1.setCode("CRZ");
-//		i1.setName("asd");		
-//		ilist.add(i1);
 		return ilist;
 	}
 	
