90faa6497b2abfa0731ad6f251d836839c85db57
Suzuwei <zuwei.su@dxc.com>
2019-10-24 14:47:48 +0800

    修改测试数据

--- bayern/src/test/java/com/qhc/bayern/service/OrderServiceTest.java
+++ bayern/src/test/java/com/qhc/bayern/service/OrderServiceTest.java
@@ -41,30 +41,30 @@ class OrderServiceTest {
 		
 		SapOrderHeader header = new SapOrderHeader();
 		sapCreationOrder.setIsZhdr(header);
-		header.setAuart("ZH0T");
-		header.setVkorg("0842");
-		header.setVtweg("1");
-		header.setName2("1");
-		header.setSpart("10");
-		header.setVkbur("C001");
-		header.setVkgrp("C05");
-		header.setVbeln("1");
-		header.setKvgr1("1");
-		header.setKvgr2("1");
-		header.setKvgr3("1");
-		header.setBstzd("1");
-		header.setBstkdE("1");
-		header.setVsart("1");
-		header.setZterm("1");
-		header.setKunnr("3001");
-		header.setWaerk("1");
-		header.setInco1("EXW");
-		header.setInco2("EXW");
-		header.setVbbkz120("1");
-		header.setVbbkz121("1");
-		header.setVbbkz109("1");
-		header.setVbbkz108("1");
-		header.setVbbkz122("1");
+//		header.setAuart("ZH0T");
+//		header.setVkorg("0842");
+//		header.setVtweg("1");
+//		header.setName2("1");
+//		header.setSpart("10");
+//		header.setVkbur("C001");
+//		header.setVkgrp("C05");
+//		header.setVbeln("1");
+//		header.setKvgr1("1");
+//		header.setKvgr2("1");
+//		header.setKvgr3("1");
+//		header.setBstzd("1");
+//		header.setBstkdE("1");
+//		header.setVsart("1");
+//		header.setZterm("1");
+//		header.setKunnr("3001");
+//		header.setWaerk("1");
+//		header.setInco1("EXW");
+//		header.setInco2("EXW");
+//		header.setVbbkz120("1");
+//		header.setVbbkz121("1");
+//		header.setVbbkz109("1");
+//		header.setVbbkz108("1");
+//		header.setVbbkz122("1");
 		
 		// Test data 2019/10/23
 		header.setAuart("ZH0T");
@@ -80,37 +80,13 @@ class OrderServiceTest {
 		
 		List<SapOrderItem> items = new ArrayList<>();
 		SapOrderItem item = new SapOrderItem();
-		item.setPosnr(10);
-		item.setMatnr("1");
-		item.setZmeng(1);
-		item.setEdatu("20191101");
-		item.setPstyv("1");
-		item.setVkaus("1");
-		item.setStreet("Dalian");
-		item.setRegion("1");
-		item.setCity1("1");
-		item.setCity2("1");
-		item.setVbbp0006("1");
-		item.setVbbpz121("1");
-		item.setVbbpz117("1");
-		item.setVbbpz120("1");
-		item.setVbbp0007("1");
-		
-		// Test data 2019/10/23
-		item.setPosnr(10);
-		item.setMatnr("BG1AM200000");
-		item.setZmeng(100);
-		
-		items.add(item);
-		
-//		item = new SapOrderItem();
-//		item.setPosnr(20);
+//		item.setPosnr(10);
 //		item.setMatnr("1");
 //		item.setZmeng(1);
-//		item.setEdatu("20191108");
+//		item.setEdatu("20191101");
 //		item.setPstyv("1");
 //		item.setVkaus("1");
-//		item.setStreet("Qingdao");
+//		item.setStreet("Dalian");
 //		item.setRegion("1");
 //		item.setCity1("1");
 //		item.setCity2("1");
@@ -119,7 +95,14 @@ class OrderServiceTest {
 //		item.setVbbpz117("1");
 //		item.setVbbpz120("1");
 //		item.setVbbp0007("1");
-//		items.add(item);
+		
+		// Test data 2019/10/23
+		item.setPosnr(10);
+		item.setMatnr("BG1AM200000");
+		item.setZmeng(100);
+		
+		items.add(item);
+
 		sapCreationOrder.setItZitem(items);
 		
 		List<SapOrderCharacteristics> characs = new ArrayList<>();
@@ -174,39 +157,34 @@ class OrderServiceTest {
 		price.setKschl("ZH05");
 		price.setKbetr(new BigDecimal("2"));
 		prices.add(price);
-//		price = new SapOrderPrice();
-//		price.setPosnr(210);
-//		price.setKschl("A2");
-//		price.setKbetr(new BigDecimal("99.99"));
-//		prices.add(price);
 		sapCreationOrder.setItZcond(prices);
 		
 		List<SapOrderPlan> plans = new ArrayList<SapOrderPlan>();
-		SapOrderPlan plan = null;
-		plan = new SapOrderPlan();
-		plan.setTetxt("Q001");
-		plan.setFkdat("20191010");
-		plan.setFakwr(BigDecimal.valueOf(3));
-		plan.setZterm("Q005");
-		plans.add(plan);
-		plan = new SapOrderPlan();
-		plan.setTetxt("Q002");
-		plan.setFkdat("20191110");
-		plan.setFakwr(BigDecimal.valueOf(4));
-		plan.setZterm("Q010");
-		plans.add(plan);
-		plan = new SapOrderPlan();
-		plan.setTetxt("Q003");
-		plan.setFkdat("20191209");
-		plan.setFakwr(BigDecimal.valueOf(5));
-		plan.setZterm("Q015");
-		plans.add(plan);
-		plan = new SapOrderPlan();
-		plan.setTetxt("Q004");
-		plan.setFkdat("20200109");
-		plan.setFakwr(BigDecimal.valueOf(6));
-		plan.setZterm("Q020");
-		plans.add(plan);
+//		SapOrderPlan plan = null;
+//		plan = new SapOrderPlan();
+//		plan.setTetxt("Q001");
+//		plan.setFkdat("20191010");
+//		plan.setFakwr(BigDecimal.valueOf(3));
+//		plan.setZterm("Q005");
+//		plans.add(plan);
+//		plan = new SapOrderPlan();
+//		plan.setTetxt("Q002");
+//		plan.setFkdat("20191110");
+//		plan.setFakwr(BigDecimal.valueOf(4));
+//		plan.setZterm("Q010");
+//		plans.add(plan);
+//		plan = new SapOrderPlan();
+//		plan.setTetxt("Q003");
+//		plan.setFkdat("20191209");
+//		plan.setFakwr(BigDecimal.valueOf(5));
+//		plan.setZterm("Q015");
+//		plans.add(plan);
+//		plan = new SapOrderPlan();
+//		plan.setTetxt("Q004");
+//		plan.setFkdat("20200109");
+//		plan.setFakwr(BigDecimal.valueOf(6));
+//		plan.setZterm("Q020");
+//		plans.add(plan);
 		sapCreationOrder.setItZplan(plans);
 		
 		return sapCreationOrder;
